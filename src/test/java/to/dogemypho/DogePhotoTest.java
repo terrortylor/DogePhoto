6196b4465b3159eacae17a687cc10074870630c3
Alex Tylor <alex@tylor.co.uk>
2014-02-19 09:48:58 +0000

    Fixed Mockito test so iterates list correctly.

--- src/test/java/to/dogemypho/DogePhotoTest.java
+++ src/test/java/to/dogemypho/DogePhotoTest.java
@@ -48,46 +48,36 @@ public class DogePhotoTest {
 
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
-	
-	@Mock Random randomGeneratorMock;
-	@Mock List<String> userDogeTextListMock;
-	@Mock Iterator<String> userDogeTextIteratorMock;
-	@Mock BufferedImage bufferedImageMock;
-	@Mock Graphics graphicsMock;
-	@Mock Font fontMock;
+	@Mock
+	Random randomGeneratorMock;
+	@Mock
+	List<String> userDogeTextListMock;
+	@Mock
+	Iterator<String> userDogeTextIteratorMock;
+	@Mock
+	BufferedImage bufferedImageMock;
+	@Mock
+	Graphics graphicsMock;
+	@Mock
+	Font fontMock;
 
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
-	    randomGeneratorMock = Mockito.mock(Random.class);
+		randomGeneratorMock = Mockito.mock(Random.class);
 		testObj = new DogePhoto();
-	
-//		userDogeTextList = new ArrayList<String>();
-//		userDogeTextList.add(WOW);
-//		userDogeTextList.add(SUCH_FUN);
-//		userDogeTextList.add(MANY_TESTS);
-//		userDogeTextList.add(SO_FUN);
-//		Mockito.doReturn(WOW).when(userDogeTextListMock).get(0);
-//		Mockito.doReturn(SUCH_FUN).when(userDogeTextListMock).get(1);
-//		Mockito.doReturn(MANY_TESTS).when(userDogeTextListMock).get(2);
-//		Mockito.doReturn(SO_FUN).when(userDogeTextListMock).get(3);
-//		userDogeTextIteratorMock = Mockito.mock(Iterator.class);
-//		Mockito.doReturn(userDogeTextIteratorMock).when(userDogeTextList).iterator();
-//		Mockito.doReturn(WOW).when(userDogeTextIteratorMock).hasNext();
-//		Mockito.doReturn(SUCH_FUN).when(userDogeTextIteratorMock).hasNext();
-//		Mockito.doReturn(MANY_TESTS).when(userDogeTextIteratorMock).hasNext();
-//		Mockito.doReturn(SO_FUN).when(userDogeTextIteratorMock).hasNext();
-		
-		
+
 		userDogeTextIteratorMock = Mockito.mock(Iterator.class);
-        Mockito.when(userDogeTextIteratorMock.hasNext()).thenReturn(true, true, true, true, false);
-        Mockito.when(userDogeTextIteratorMock.next()).thenReturn(WOW)
-            .thenReturn(SUCH_FUN).thenReturn(MANY_TESTS).thenReturn(SO_FUN);
+		Mockito.when(userDogeTextIteratorMock.hasNext()).thenReturn(true, true,
+				true, true, false);
+		Mockito.when(userDogeTextIteratorMock.next()).thenReturn(WOW)
+				.thenReturn(SUCH_FUN).thenReturn(MANY_TESTS).thenReturn(SO_FUN);
 
-        userDogeTextListMock = Mockito.mock(List.class);
-        Mockito.when(userDogeTextListMock.iterator()).thenReturn(userDogeTextIteratorMock);
+		userDogeTextListMock = Mockito.mock(List.class);
+		Mockito.when(userDogeTextListMock.iterator()).thenReturn(
+				userDogeTextIteratorMock);
 
-        bufferedImageMock = Mockito.mock(BufferedImage.class);
+		bufferedImageMock = Mockito.mock(BufferedImage.class);
 	}
 
 	@Test
@@ -109,9 +99,11 @@ public class DogePhotoTest {
 	@Test
 	public void testDogeMyPhoto_Zero_orLess_FontPointSize() throws Exception {
 		exception.expect(Exception.class);
-		exception.expectMessage(DogePhoto.FONT_POINT_SIZE_SHOULD_BE_GREATER_THAN_0);
+		exception
+				.expectMessage(DogePhoto.FONT_POINT_SIZE_SHOULD_BE_GREATER_THAN_0);
 
-		testObj.dogeMyPhoto(INVALID_FILENAME_JPG, 0, Collections.<String> emptyList());
+		testObj.dogeMyPhoto(INVALID_FILENAME_JPG, 0,
+				Collections.<String> emptyList());
 	}
 
 	@Test
@@ -127,111 +119,110 @@ public class DogePhotoTest {
 		exception.expect(Exception.class);
 		exception.expectMessage(DogePhoto.EMPTY_TEXT_LIST);
 
-		testObj.dogeMyPhoto(INVALID_FILENAME_JPG, 10, Collections.<String> emptyList());
+		testObj.dogeMyPhoto(INVALID_FILENAME_JPG, 10,
+				Collections.<String> emptyList());
 	}
 
 	@Test
-	public void testDogeMyPhoto_Iterate_All_List() throws Exception{
+	public void testDogeMyPhoto_Iterate_All_List() throws Exception {
 		testObj.setRandomGenerator(new Random());
 
-		Mockito.when(userDogeTextIteratorMock.next()).thenReturn(WOW);
-		Mockito.when(userDogeTextIteratorMock.next()).thenReturn(SUCH_FUN);
-		Mockito.when(userDogeTextIteratorMock.next()).thenReturn(MANY_TESTS);
-		Mockito.when(userDogeTextIteratorMock.next()).thenReturn(SO_FUN);
+		Mockito.when(userDogeTextIteratorMock.next()).thenReturn(WOW)
+				.thenReturn(SUCH_FUN).thenReturn(MANY_TESTS).thenReturn(SO_FUN);
 
 		testObj.dogeMyPhoto(VALID_FILENAME, FONT_POINT_80, userDogeTextListMock);
 
 		Mockito.verify(userDogeTextIteratorMock, Mockito.times(4)).next();
 		Mockito.verify(userDogeTextIteratorMock, Mockito.times(5)).hasNext();
 	}
-	
+
 	@Test
 	public void testCheckForCollision_B_Left_Collision() {
 		DogeText a = new DogeText(WOW, 10, 10, 15, 10);
 		DogeText b = new DogeText(WOW, 15, 11, 15, 10);
-		
+
 		boolean result = testObj.checkForCollision(a, b);
-		
+
 		assertTrue("Collision expected", result);
 	}
-	
+
 	@Test
 	public void testCheckForCollision_B_Left_No_Collision() {
 		DogeText a = new DogeText(WOW, 10, 10, 15, 10);
 		DogeText b = new DogeText(WOW, 15, 30, 15, 10);
-		
+
 		boolean result = testObj.checkForCollision(a, b);
-		
+
 		assertFalse("Collision not expected", result);
 	}
-	
+
 	@Test
 	public void testCheckForCollision_B_Right_Collision() {
 		DogeText a = new DogeText(WOW, 10, 10, 15, 10);
 		DogeText b = new DogeText(WOW, 5, 11, 15, 10);
-		
+
 		boolean result = testObj.checkForCollision(a, b);
-		
+
 		assertTrue("Collision expected", result);
 	}
-	
+
 	@Test
 	public void testCheckForCollision_B_Right_No_Collision() {
 		DogeText a = new DogeText(WOW, 10, 10, 15, 10);
 		DogeText b = new DogeText(WOW, 5, 30, 15, 10);
-		
+
 		boolean result = testObj.checkForCollision(a, b);
-		
+
 		assertFalse("Collision expected", result);
 	}
-	
+
 	@Test
 	public void testCheckForCollision_B_Top_Collision() {
 		DogeText a = new DogeText(WOW, 10, 10, 15, 10);
 		DogeText b = new DogeText(WOW, 15, 5, 15, 10);
-		
+
 		boolean result = testObj.checkForCollision(a, b);
-		
+
 		assertTrue("Collision expected", result);
 	}
-	
+
 	@Test
 	public void testCheckForCollision_B_Top_No_Collision() {
 		DogeText a = new DogeText(WOW, 10, 10, 15, 10);
 		DogeText b = new DogeText(WOW, 30, 5, 15, 10);
-		
+
 		boolean result = testObj.checkForCollision(a, b);
-		
+
 		assertFalse("Collision not expected", result);
 	}
-	
+
 	@Test
 	public void testCheckForCollision_B_Bottom_Collision() {
 		DogeText a = new DogeText(WOW, 10, 10, 15, 10);
 		DogeText b = new DogeText(WOW, 5, 5, 15, 10);
-		
+
 		boolean result = testObj.checkForCollision(a, b);
-		
+
 		assertTrue("Collision expected", result);
 	}
-	
+
 	@Test
 	public void testCheckForCollision_B_Bottom_No_Collision() {
 		DogeText a = new DogeText(WOW, 10, 10, 15, 10);
 		DogeText b = new DogeText(WOW, 30, 5, 15, 10);
-		
+
 		boolean result = testObj.checkForCollision(a, b);
-		
+
 		assertFalse("Collision not expected", result);
 	}
-	
+
 	@Test
 	public void testCheckForCollision_B_in_A() {
 		DogeText a = new DogeText(WOW, 10, 10, 40, 40);
 		DogeText b = new DogeText(WOW, 15, 15, 5, 5);
-		
+
 		boolean result = testObj.checkForCollision(a, b);
-		
+
 		assertTrue("Collision expected", result);
 	}
 
@@ -249,10 +240,13 @@ public class DogePhotoTest {
 		int imageHeightMinusTextHeight = 195;
 
 		testObj.setRandomGenerator(randomGeneratorMock);
-		Mockito.when(randomGeneratorMock.nextInt(imageWidthMinusTextWidth)).thenReturn(x);
-		Mockito.when(randomGeneratorMock.nextInt(imageHeightMinusTextHeight)).thenReturn(y);
-	
-		DogeText result = testObj.createNewText(WOW, imageWidth, imageHeight, textWidth, textHeight);
+		Mockito.when(randomGeneratorMock.nextInt(imageWidthMinusTextWidth))
+				.thenReturn(x);
+		Mockito.when(randomGeneratorMock.nextInt(imageHeightMinusTextHeight))
+				.thenReturn(y);
+
+		DogeText result = testObj.createNewText(WOW, imageWidth, imageHeight,
+				textWidth, textHeight);
 
 		assertEquals(x, result.getLeft());
 		assertEquals(y, result.getTop());
@@ -273,7 +267,7 @@ public class DogePhotoTest {
 	@Test
 	public void testLoadImage_Correct_FileName() throws IOException {
 		BufferedImage result = testObj.loadImage(VALID_FILENAME);
-	
+
 		assertNotNull(result);
 	}
 
@@ -294,7 +288,8 @@ public class DogePhotoTest {
 	public void testGetTextMetrics_withMoreText() throws IOException {
 		BufferedImage image = testObj.loadImage(VALID_FILENAME);
 
-		int[] result = testObj.getTextMetrics(image, SOME_LONGER_TEXT, FONT_POINT_80);
+		int[] result = testObj.getTextMetrics(image, SOME_LONGER_TEXT,
+				FONT_POINT_80);
 
 		assertNotNull(result);
 		assertEquals(-1, Integer.compare(0, result[0]));
@@ -302,7 +297,6 @@ public class DogePhotoTest {
 		assertEquals(654, result[0]);
 		assertEquals(81, result[1]);
 	}
-
 
 	@Test
 	public void testGetTextMetrics_withText_Smaller_Font() throws IOException {
@@ -339,12 +333,12 @@ public class DogePhotoTest {
 		Mockito.when(randomGeneratorMock.nextFloat()).thenReturn(0f);
 		Mockito.when(randomGeneratorMock.nextFloat()).thenReturn(0f);
 
-		
 		testObj.addTextToImage(bufferedImageMock, dogeText, FONT_POINT_80);
-		
+
 		Mockito.verify(bufferedImageMock).getGraphics();
-		Mockito.verify(graphicsMock).drawString(dogeText.getText(), dogeText.getLeft(), dogeText.getTop());
+		Mockito.verify(graphicsMock).drawString(dogeText.getText(),
+				dogeText.getLeft(), dogeText.getTop());
 		Mockito.verify(graphicsMock).dispose();
 	}
-	
+
 }
